99fb3d6ff4fc91c6d7cdd619789793f8ff6ece3a
Thorzila <juancamilo106055@gmail.com>
2021-04-08 04:11:25 +0000

    Created SampleCategoryAttribute but not working at all, added to adminhtml

---
+++ app/code/OmniPro/SampleCategoryAttribute/Setup/InstallData.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OmniPro\SampleDataPatch\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+
+class InstallData implements InstallDataInterface
+{
+
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    /**
+     * @param ModuleDataSetupInterface $setup
+     * @param ModuleContextInterface $context
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Category::ENTITY,
+            'sample_category_attribute',
+            [
+                'type' => 'varchar',
+                'label' => 'Sample Category Attribute',
+                'input' => 'image',
+                'source' => '',
+                'global' => 1,
+                'visible' => true,
+                'required' => false,
+                'user_defined' => false,
+                'default' => null,
+                'group' => '',
+                'backend' => ''
+            ]
+        );
+    }
+}
