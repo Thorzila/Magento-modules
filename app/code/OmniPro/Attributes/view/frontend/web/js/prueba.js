a160cc46d12bd62cfea50d56d7aa4d0e82c849be
Thorzila <juancamilo106055@gmail.com>
2021-04-12 14:09:23 +0000

    update blog list

--- app/code/OmniPro/Attributes/view/frontend/web/js/prueba.js
+++ app/code/OmniPro/Attributes/view/frontend/web/js/prueba.js
@@ -1,14 +1,16 @@
 define([
-    'jquery',
-    'underscore'
-], function($, _) {
-    return (config) => {
-        console.log(config['prueba-parametro']);
-        var blogs = "/rest/V1/blogs?searchCriteria";
-        $.ajax({
-            url : blogs
-        }).done((resp)=>{
-            console.log(resp);
+    'jquery'
+], function($) {;
+    var blogs = "/rest/V1/blogs?searchCriteria";
+    $.getJSON({
+        url : blogs
+    }).done((resp)=>{
+        var array = resp.items;
+        array.forEach(item => {
+            $("#content").append(item.title + "<br>");
+            $("#content").append(item.content + "<br>");
+            $("#content").append(item.email + "<br>");
+            $("#content").append("<br>");
         });
-    }
+    });
 });
