48bb02a560df7cd112b2b70fed953f905075e4e7
Thorzila <juancamilo106055@gmail.com>
2021-04-13 14:31:09 +0000

    Blog list gird wo

--- app/code/OmniPro/Attributes/view/frontend/web/js/prueba.js
+++ app/code/OmniPro/Attributes/view/frontend/web/js/prueba.js
@@ -1,7 +1,37 @@
 define([
-    'jquery'
-], function($) {;
-    var blogs = "/rest/V1/blogs?searchCriteria";
+    'jquery',
+    'uiComponent',
+    'ko',
+    'mage/url',
+    'mage/storage',
+    'Magento_Customer/js/customer-data'
+], function($, Component, ko, url, storage, customerData) {
+    return Component.extend({
+        defaults: {
+            blogs:ko.observableArray([]),
+            template:'OmniPro_Attributes/blog',
+            tracks: {
+                segundaVariable:false
+            }
+        },
+        initialize: function () {
+            this._super();
+            console.log(this);
+            console.log(this.tracks.segundaVariable);
+            var self = this;
+            console.log(url.build('omniproattributes/prueba/omnipro'));
+            //console.log(customerData.get('cart')());
+            var blogs = "/rest/V1/blogs?searchCriteria";
+            storage.get(blogs)
+            .done((resp)=>{
+                self.blogs(resp.items);
+                console.log(self.blogs());
+            });
+            return this;
+        }
+    });
+    
+    /*var blogs = "/rest/V1/blogs?searchCriteria";
     $.getJSON({
         url : blogs
     }).done((resp)=>{
@@ -12,5 +42,5 @@ define([
             $("#content").append(item.email + "<br>");
             $("#content").append("<br>");
         });
-    });
+    });*/
 });
