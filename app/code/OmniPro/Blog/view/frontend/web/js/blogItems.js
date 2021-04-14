7b454a3fe095cb34e2ba4870d576b8c88fb443b6
Thorzila <juancamilo106055@gmail.com>
2021-04-14 23:40:18 +0000

    Save blog from form using API

--- app/code/OmniPro/Blog/view/frontend/web/js/blogItems.js
+++ app/code/OmniPro/Blog/view/frontend/web/js/blogItems.js
@@ -1,29 +1,63 @@
 define([
     'uiComponent',
     'ko',
-    'jquery'
-], function(Component, ko, $){
+    'jquery',
+    'mage/storage',
+    'mage/url'
+], function(Component, ko, $, storage, url){
+    url.setBaseUrl(window.BASE_URL);
     return Component.extend({
         defaults: {
-            textoPrueba: "Texto Prueba"
+            title: '',
+            content: '',
+            email: '',
+            img: '',
+            blogs: [],
+            blogsUrl: 'rest/V1/blogs?searchCriteria',
+            blogPostUrl: 'rest/V1/blogs'
         },
         initObservable: function(){
             this._super()
                 .observe([
-                    'textoPrueba'
-                ]);
+                    'title',
+                    'content',
+                    'email',
+                    'img'
+                ])
+                .observe({
+                    blogs: []
+                });
             return this;
         },
         initialize: function() {
             this._super();
-            //console.log(this);
-            var self = this;
-            console.log(self);
-            setTimeout($.proxy(function(){
-                this.textoPrueba("Prueba 2");
-                console.log(this)
-            }, this), 1000);
+            this.getBlogs();
+            $(document).ready(function(){
+                $("#saveButton").click(function(){
+                    window.location.reload();
+                });
+            })
             return this;
+        },
+        saveBlog: function() {
+            var blog = {
+                'blog': {
+                    "title": this.title(),
+                    "email": this.email(),
+                    "content": this.content(),
+                    "img": this.img()
+                }
+            };
+            storage.post(this.blogPostUrl, JSON.stringify(blog))
+            .then($.proxy(function() {
+                this.getBlogs();
+            }, this));
+        },
+        getBlogs: function() {
+            storage.get(this.blogsUrl)
+            .then($.proxy(function(resp) {
+                this.blogs(resp.items);
+            },this));
         }
     });
 });
