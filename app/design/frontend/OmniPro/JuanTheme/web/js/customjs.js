0f9b3f8870587a5e41bcdb58852b5e9f278d1faf
Thorzila <juancamilo106055@gmail.com>
2021-04-28 00:34:19 +0000

    Working on product view styles

--- app/design/frontend/OmniPro/JuanTheme/web/js/customjs.js
+++ app/design/frontend/OmniPro/JuanTheme/web/js/customjs.js
@@ -1,1 +1,16 @@
-console.log('Hola Mundooooo');
+function signin() {
+
+    var login = document.getElementById("login-form");
+    var register = document.getElementById("form-validate");
+    
+    login.style.display = "block";
+    register.style.display = "none";
+}
+
+function signup() {
+    var login = document.getElementById("login-form");
+    var register = document.getElementById("form-validate");
+
+    login.style.display = "none";
+    register.style.display = "block";
+}
