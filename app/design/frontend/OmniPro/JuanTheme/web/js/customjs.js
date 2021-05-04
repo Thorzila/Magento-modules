de04ee86a5795b26cc7894f401c2346740191360
Juan <juancamilo106055@gmail.com>
2021-05-04 09:03:04 -0500

    Trabajando en navbar, slider y detalles product page

--- app/design/frontend/OmniPro/JuanTheme/web/js/customjs.js
+++ app/design/frontend/OmniPro/JuanTheme/web/js/customjs.js
@@ -23,4 +23,59 @@ function signup() {
     register.style.display = "block";
 }
 
+function toggleText() {
+    var points = document.getElementById("points");
+    var moreText = document.getElementById("moreText");
+    var textBtn = document.getElementById("textBtn");
+
+    if (points.style.display === "none") {
+        moreText.style.display = "none";
+        points.style.display = "inline";
+        textBtn.innerHTML = "LEER MAS";
+    } else {
+        moreText.style.display = "inline";
+        points.style.display = "none";
+        textBtn.innerHTML = "LEER MENOS";
+    }
+}
+
+// require([
+//     'jquery',
+//     'slick'
+// ], function ($) {
+//     jQuery(document).ready(function () {
+//         jQuery(".widget-product-grid").slick({
+//             dots: true,
+//             infinite: true,
+//             speed: 300,
+//             slidesToShow: 4,
+//             slidesToScroll: 4,
+//             responsive: [
+//                 {
+//                     breakpoint: 1280,
+//                     settings: {
+//                         slidesToShow: 3,
+//                         slidesToScroll: 3
+//                     }
+//                 },
+//                 {
+//                     breakpoint: 768,
+//                     settings: {
+//                         slidesToShow: 2,
+//                         slidesToScroll: 2
+//                     }
+//                 },
+//                 {
+//                     breakpoint: 600,
+//                     settings: {
+//                         slidesToShow: 1,
+//                         slidesToScroll: 1
+//                     }
+//                 }
+//             ]
+//         });
+//     });
+// });
+
+
 
